0126fd8365942bc0373f24feafe0e63094d1b567
sho037 <103867061+sho037@users.noreply.github.com>
2023-06-16 14:42:28 +0900

    ✨ ステータスを出力できるように

--- src/main.c
+++ src/main.c
@@ -2,10 +2,27 @@
 #include "../include/CheckScreenSize.h"
 #include "../include/EndProcess.h"
 
+void printStatus(int status_code)
+{
+  if (status_code == 0)
+  {
+    printf("Success.\n");
+  }
+  else
+  {
+    printf("Failed.\n");
+  }
+}
+
 int main(int argc, char const *argv[])
 {
   /* code */
-  checkScreenSize();
-  end();
+  int status_code;
+  status_code = checkScreenSize();
+  if (status_code != 0)
+  {
+    printStatus(status_code);
+  }
+  endProcess();
   return 0;
 }
