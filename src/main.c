e26dd8926b05178ed32e0beaaf05a12626fea3f9
sho037 <103867061+sho037@users.noreply.github.com>
2023-07-07 10:07:43 +0900

    ✨ 実際にjsonからデータを受け取り表示するように変更

--- src/main.c
+++ src/main.c
@@ -24,7 +24,7 @@ void endProcess()
   StatusData.jsonを読み込み、その中身を返す
   大域変数status_codeによって返す内容を変更する
 */
-char *getStatusData(){
+const char *getStatusData(){
   // JSONオブジェクトを入れる変数
   json_error_t jerror;
   json_t* root;
@@ -44,11 +44,48 @@ char *getStatusData(){
     exit(1);
   }
 
-  int test = json_integer_value(json_object_get(root, "test"));
+  // "StatusData"配列を取得
+  json_t* status_data = json_object_get(root, "StatusData");
+  if (!json_is_array(status_data)){
+    fprintf(stderr, "error: in getStatusData: JSONパースエラー: StatusDataが配列ではないもしくは見つかりません\n");
+    fclose(file);
+    endProcess();
+    exit(1);
+  }
+
+  size_t index;
+  json_t* value;
+  // status_codeによって返す内容を変更する
+  json_array_foreach(status_data, index, value){
+    // StatusCodeを取得
+    json_t* status_code_json = json_object_get(value, "StatusCode");
+    if (!json_is_integer(status_code_json)){
+      fprintf(stderr, "error: in getStatusData: JSONパースエラー: StatusCodeが整数ではありません\n");
+      fclose(file);
+      endProcess();
+      exit(1);
+    }
+
+    // status_codeが一致した場合、その内容を返す
+    if (json_integer_value(status_code_json) == status_code){
+      json_t* status_message_json = json_object_get(value, "Message");
+      if (!json_is_string(status_message_json)){
+        fprintf(stderr, "error: in getStatusData: JSONパースエラー: Messageが文字列ではありません\n");
+        fclose(file);
+        endProcess();
+        exit(1);
+      }
+
+      fclose(file);
+
+      const char *status_message = json_string_value(status_message_json);
+      return status_message;
+    }
+  }
 
   fclose(file);
 
-  return ;
+  return "test";
 }
 
 ScreenSize getScreenSizeFromConfig()
