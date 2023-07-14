eefafee977356aa0535f69c22387a6495f67c622
sho037 <103867061+sho037@users.noreply.github.com>
2023-07-14 13:38:33 +0900

    ✨ ステータスを保存する機能を作成

---
+++ include/common/StatusData.hpp
@@ -0,0 +1,31 @@
+/**
+ * @file StatusData.hpp
+ * @brief ステータスデータを管理するクラス
+ * @details status_codeとmessageを管理する
+*/
+
+#include <vector>
+
+class StatusData
+{
+private:
+  /**
+   * ステータスデータの構造体
+   */
+  struct StatusData
+  {
+    int status_code;
+    char *message;
+  };
+
+  /**
+   * ステータスデータを格納する配列
+   */
+  std::vector<StatusData> status_data_list;
+
+public:
+  StatusData(/* args */);
+  ~StatusData();
+  void addStatusData(const char *status_code, const char *message);
+  bool hasErrorStatus();
+};
---
+++ src/common/StatusData.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <algorithm>
+#include "../../include/common/StatusData.hpp"
+
+/**
+ * ステータスデータを追加する
+ * @param status_code ステータスコード
+ * @param message メッセージ
+ * @return なし
+*/
+void addStatusData(int status_code, char *message)
+{
+  StatusData status_data;
+  status_data.status_code = status_code;
+  status_data.message = message;
+  status_data_list.push_back(status_data);
+}
+
+/**
+ * エラーステータスがstatus_data_listに存在するかどうかを返す
+ * エラーステータスはマイナスの値をとる
+*/
+bool hasErrorStatus()
+{
+  for (auto status_data : status_data_list)
+  {
+    if (status_data.status_code < 0)
+    {
+      return true;
+    }
+  }
+  return false;
+}
